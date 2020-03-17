08016deb72bce36b69dcb4bd78104a747b04dc81
mubeenahmad571 <mubeenahmad571@gmail.com>
2020-03-17 08:57:13 +0500

    Initial commit

---
+++ app/src/main/java/com/mubeen/assignment_one/Student.java
@@ -0,0 +1,15 @@
+package com.mubeen.assignment_one;
+
+public class Student {
+
+    private String name;
+    private Subject roollno;
+
+    private Subject subobj;
+
+    public Student(String name, Subject roollno, Subject subobj) {
+        this.name = name;
+        this.roollno = roollno;
+        this.subobj = subobj;
+    }
+}
---
+++ app/src/main/java/com/mubeen/assignment_one/Subject.java
@@ -0,0 +1,15 @@
+package com.mubeen.assignment_one;
+
+
+public class Subject {
+
+
+
+    private String subjectname;
+    private  int Credit_Houre ;
+
+    public Subject(String subjectname, int credit_Houre) {
+        this.subjectname = subjectname;
+        Credit_Houre = credit_Houre;
+    }
+}
